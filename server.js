63c02d64d994f08a5e0f9b3f2ae67485844c4102
Oggy107 <urmalsingh10@gmail.com>
2022-01-17 16:23:11 +0530

    Implimented: db connection

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('./config');
+const connectDB = require('./db/connectDB');
 require('dotenv').config();
 
 const notFound = require('./middleware/notFound');
@@ -7,9 +8,23 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 const PORT = config.server.port;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use([notFound, errorHandler]);
 
-app.listen(PORT, () => {
-    console.log('[SERVER]: running at http://localhost:' + PORT);
-})
+const main = async () => {
+    try {
+        await connectDB(MONGO_URI);
+        console.log('[SERVER]: connected to db successfully');
+
+        app.listen(PORT, () => {
+            console.log('[SERVER]: running at http://localhost:' + PORT);
+        });
+    } catch (error) {
+        console.error('[ERROR]: ', error);
+        process.exitCode = 1;
+    }
+}
+
+if (require.main == module)
+    main();
