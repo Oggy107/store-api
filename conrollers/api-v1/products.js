4149cf798fd4aa4d60111e34b6a46fd635311f16
Oggy107 <urmalsingh10@gmail.com>
2022-01-23 12:00:01 +0530

    Refactored: query handler

--- conrollers/api-v1/products.js
+++ conrollers/api-v1/products.js
@@ -1,8 +1,25 @@
+const { query } = require('express');
 const product = require('../../models/products');
 
 const getAllProducts = async (req, res) => {
+    const {featured, company, name} = req.query;
+    const queryObject = {};
+
+    if (featured)
+    {
+        queryObject.featured = featured;
+    }
+    if (company)
+    {
+        queryObject.company = company;
+    }
+    if (name)
+    {
+        queryObject.name = {$regex: name, $options: 'i'};
+    }
+
     try {
-        const products = await product.find(req.query)
+        const products = await product.find(queryObject);
         res.json({sucess: true, data: products});
     } catch (error) {
         throw {status: 400, msg: error.message};
